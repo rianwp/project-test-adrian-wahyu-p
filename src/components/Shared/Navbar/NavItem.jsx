9e05d1395ffdd7c8f5c45582b5647bee67d61731
rianwp <rianwp2015@gmail.com>
2023-12-23 04:03:37 +0700

    adding sidebar

--- src/components/Shared/Navbar/NavItem.jsx
+++ src/components/Shared/Navbar/NavItem.jsx
@@ -1,16 +1,16 @@
 import { Link, useLocation } from "react-router-dom"
 
-const NavItem = ({ href, name }) => {
+const NavItem = ({ href, name, sidebar }) => {
 	const location = useLocation()
 	const isActive = location.pathname === href
 
 	return (
 		<Link to={href} className="flex flex-col gap-0.5">
-			<p className="text-white">{name}</p>
+			<p>{name}</p>
 			<div
-				className={`bg-white w-full h-1 rounded-full ${
-					isActive ? "block" : "hidden"
-				}`}
+				className={`${
+					sidebar ? "bg-black w-full" : "bg-white w-full"
+				} h-1 rounded-full ${isActive ? "block" : "hidden"}`}
 			/>
 		</Link>
 	)
--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -3,8 +3,11 @@ import Brand from "../../Brand"
 import Container from "../../Container"
 import { navItems } from "../../../lib/navItems"
 import NavItem from "./NavItem"
+import menu from "../../../assets/menu.svg"
+import chevron_right from "../../../assets/chevron-right.svg"
 
 const Navbar = () => {
+	const [open, setOpen] = useState(false)
 	const [show, setShow] = useState(true)
 	const [currentScrollY, setCurrentScrollY] = useState(window.scrollY)
 
@@ -22,24 +25,56 @@ const Navbar = () => {
 	}, [currentScrollY])
 
 	return (
-		<div
-			className={`fixed top-0 left-0 w-full h-16 transition duration-300 z-50 ${
-				show ? "translate-y-0" : "-translate-y-16"
-			} ${
-				currentScrollY === 0 ? "bg-primary" : "bg-primary/60 backdrop-blur-sm"
-			}`}
-		>
-			<Container className="flex justify-between items-center h-full">
-				<Brand />
-				<div className="flex flex-row gap-8">
+		<>
+			<div
+				className={`fixed top-0 left-0 w-full h-16 transition duration-300 z-40 ${
+					show ? "translate-y-0" : "-translate-y-16"
+				} ${
+					currentScrollY === 0 ? "bg-primary" : "bg-primary/60 backdrop-blur-sm"
+				}`}
+			>
+				<Container className="flex justify-between items-center h-full">
+					<Brand />
+					<div>
+						<div className="md:flex flex-row gap-8 hidden text-white">
+							{navItems.map((navItem, index) => {
+								return (
+									<NavItem
+										key={index}
+										href={navItem.href}
+										name={navItem.name}
+									/>
+								)
+							})}
+						</div>
+						<button className="md:hidden block" onClick={() => setOpen(true)}>
+							<img src={menu} className="w-6 h-6" />
+						</button>
+					</div>
+				</Container>
+			</div>
+			<div
+				className={`md:w-64 w-full fixed h-screen md:-right-64 -right-full bg-white top-0 ${
+					open ? "-translate-x-64" : "translate-x-0"
+				} transition duration-300 z-50`}
+			>
+				<div className="w-fit p-4 flex flex-col gap-4">
+					<button onClick={() => setOpen(false)}>
+						<img src={chevron_right} className="w-5 h-5" />
+					</button>
 					{navItems.map((navItem, index) => {
 						return (
-							<NavItem key={index} href={navItem.href} name={navItem.name} />
+							<NavItem
+								sidebar
+								key={index}
+								href={navItem.href}
+								name={navItem.name}
+							/>
 						)
 					})}
 				</div>
-			</Container>
-		</div>
+			</div>
+		</>
 	)
 }
 
